9ffffb7c5ccdec5f9144bc9ac0e25a2a55fb8716
nguyentuananh04 <tanh22012004@gmail.com>
2024-11-29 09:39:56 +0100

    roll 20 fois 0

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,11 +1,19 @@
 package Game;
 
 public class Game {
+	private int rolls[] = new int[20];
+	private int currentRoll = 0;
+	private int score = 0;
+	
 	void roll(int nbQuilles) {
-		
+		rolls[currentRoll] = nbQuilles;
+		currentRoll++;
 	}
 	
 	int score() {
-		return 0;
+		for (int i = 0; i < rolls.length; i++) {
+			score += rolls[i];
+		}
+		return score;
 	}
 }
--- src/Game/GameTest.java
+++ src/Game/GameTest.java
@@ -9,25 +9,32 @@ import org.junit.jupiter.api.BeforeEach;
 
 class GameTest {
 	Game game;
-	int score;
 	@BeforeEach
 	void initGame() {
 		game = new Game();
-		score = 0;
 	}
 	
 	@Test
-	void rollVide() {
+	void testLevel1Cas1() {
 		assertEquals(game.score(), 0);
 		System.out.println("Entrée : vide\nScore attendu: 0\n");
 	}
 	
 	@Test
-	void roll20fois0() {
+	void testLevel1Cas2() {
 		for (int i = 0; i < 20; i++) {
 			game.roll(0);
 		}
 		assertEquals(game.score(), 0);
 		System.out.println("Entrée : On roll 20 fois 0\nScore attendu: 0\n");
+	}
+	
+	@Test
+	void testLevel1Cas3() {
+		for (int i = 0; i < 20; i++) {
+			game.roll(1);
+		}
+		assertEquals(game.score(), 20);
+		System.out.println("Entrée : On roll 20 fois 1\nScore attendu: 20\n");
 	}
 }
